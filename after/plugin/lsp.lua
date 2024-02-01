e62eb363f53be6858c822453416ed86589df115a
TeeJal <temesklk@gmail.com>
2024-02-01 10:06:32 +0200

    filter out React index.d.ts and node_modules from lsp go to definition

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -31,10 +31,44 @@ lsp.setup_nvim_cmp({
     mapping = cmp_mappings
 })
 
+local function filter(arr, fn)
+  if type(arr) ~= "table" then
+    return arr
+  end
+
+  local filtered = {}
+  for k, v in pairs(arr) do
+    if fn(v, k, arr) then
+      table.insert(filtered, v)
+    end
+  end
+
+  return filtered
+end
+
+local function filterReactDTS(value)
+  return string.match(value.filename, 'react/index.d.ts') == nil
+end
+
+local function filterNodeModules(value)
+  return string.match(value.filename, 'node_modules') == nil
+end
+
+local function on_list(options)
+  local items = options.items
+  if #items > 1 then
+    items = filter(items, filterReactDTS)
+    items = filter(items, filterNodeModules)
+  end
+
+  vim.fn.setqflist({}, ' ', { title = options.title, items = items, context = options.context })
+  vim.api.nvim_command('cfirst') -- or maybe you want 'copen' instead of 'cfirst'
+end
+
 lsp.on_attach(function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition{on_list=on_list} end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
@@ -50,6 +84,12 @@ lsp.setup()
 
 vim.diagnostic.config({
     virtual_text = true,
+})
+
+local lspconfig = require("lspconfig")
+
+lspconfig.tsserver.setup({ 
+  root_dir = lspconfig.util.root_pattern(".git")
 })
 
 -- LSP format on save
